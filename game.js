7d3a6a604914920f1c7aa65921a00a2f442668dc
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2021-04-25 16:42:08 -0700

    Buttons
    New `button()` function to create buttons.
    However, you can't click on them yet.
    - `button()` function.
    - Settled on a name (Deep Learning: The Game).
    - Added button graphics.
    - Added `clear()` to shorten text amount.
    - Removed "burp".
    - Moved state machine to bottom

--- game.js
+++ game.js
@@ -8,6 +8,25 @@ canvas.width = gameSize.x;
 canvas.height = gameSize.y;
 let canvasContext = canvas.getContext("2d");
 canvasContext.imageSmoothingEnabled = false;
+// Helper functions
+function clear() {
+	canvasContext.clearRect(0, 0, gameSize.x, gameSize.y);
+}
+function button(x, y, text, callback, width) {
+	canvasContext.font = "81.92px \"Commodore 64\", \"Roboto Slab\", \"Century Gothic\", AppleGothic, sans-serif";
+	console.log(canvasContext.measureText(text).width);
+	let buttonWidth = Math.ceil(canvasContext.measureText(text).width / 32) * 32;
+	if (width) {
+		buttonWidth = width - 160;
+	}
+	canvasContext.drawImage(cache.buttonStart, x - buttonWidth / 2 - 80, y, 80, 128);
+	canvasContext.drawImage(cache.buttonMiddle, x - buttonWidth / 2, y, buttonWidth, 128);
+	canvasContext.drawImage(cache.buttonEnd, x + buttonWidth / 2, y, 80, 128);
+	canvasContext.textAlign = "center";
+	canvasContext.fillStyle = "rgb(0, 0, 0)";
+	canvasContext.fillText(text, x, y + 92);
+	addEventListener("onclick", callback);
+}
 // Loading assets
 let cache = {};
 function loadResources(images, sounds) {
@@ -19,7 +38,7 @@ function loadResources(images, sounds) {
 		cache[path].src = folder + path + extension;
 	};
 	let success = function () {
-		successes ++;
+		successes++;
 		console.log(this.tagName + " " + this.src.split("/")[this.src.split("/").length - 1] + " has loaded; total " + successes + " successes.");
 		if (successes == images.length + sounds.length) {
 			stateMachine.ready();
@@ -32,8 +51,8 @@ function loadResources(images, sounds) {
 		initialize("audio", "canplaythrough", "sounds/", assetName, ".mp3");
 	})
 }
-const images = ["start"];
-const sounds = ["goldbergAria", "burp"];
+const images = ["start", "buttonStart", "buttonMiddle", "buttonEnd"];
+const sounds = ["goldbergAria"];
 // Noting input
 let keysPressed = {};
 let clicked = false;
@@ -63,64 +82,11 @@ addEventListener("mouseup", function (e) {
 	clicked = false;
 	console.log("The mouse was released.");
 });
-// State machine
-let stateMachine = new StateMachine({
-	init: "booting",
-	transitions: [
-		{name: "ready", from: "booting", to: "menu"},
-		{name: "pause", from: "menu", to: "paused"},
-		{name: "unpause", from: "paused", to: "menu"},
-	],
-	methods: {
-		onTransition: function (lifecycle) {
-			console.log("State: " + lifecycle.transition);
-		},
-		onBooting: function () {
-			canvasContext.rect(0, 0, gameSize.x, gameSize.y);
-			canvasContext.fillStyle = "rgb(0, 0, 0)";
-			canvasContext.fill();
-			canvasContext.font = "240px \"Commodore 64\", \"Roboto Slab\", \"Century Gothic\", AppleGothic, sans-serif";
-			canvasContext.textAlign = "center";
-			canvasContext.fillStyle = "rgb(255, 255, 255)";
-			canvasContext.fillText("LOADING", gameSize.x / 2, gameSize.y / 2);
-		},
-		onReady: function () {
-			console.log(cache);
-			canvasContext.clearRect(0, 0, gameSize.x, gameSize.y);
-			canvasContext.drawImage(cache.start, 0, 0, gameSize.x, gameSize.y);
-			loop();
-		},
-		onPause: function () {
-			canvasContext.rect(0, 0, gameSize.x, gameSize.y);
-			canvasContext.fillStyle = "rgba(0, 0, 0, 0.5)";
-			canvasContext.fill();
-			canvasContext.fillStyle = "rgb(255, 255, 255)";
-			canvasContext.fillText("PAUSED", gameSize.x / 2, gameSize.y / 2);
-			sounds.forEach(function (assetName, index) {
-				if (!cache[assetName].paused) {
-					cache[assetName].pause();
-					pausedAudio[index] = true;
-					console.log("PausedAudio[" + index + "], " + assetName + ", is now true.");
-				}
-			})
-		},
-		onUnpause: function () {
-			canvasContext.clearRect(0, 0, gameSize.x, gameSize.y);
-			sounds.forEach(function (assetName, index) {
-				if (pausedAudio[index]) {
-					cache[assetName].play();
-					pausedAudio[index] = false;
-					console.log("PausedAudio[" + index + "], " + assetName + ", is now false.");
-				}
-			})
-		}
-	}
-});
 // Game loop
 function loop() {
 	checkPause();
 	if (!stateMachine.is("paused")) {
-		canvasContext.clearRect(0, 0, gameSize.x, gameSize.y);
+		clear();
 		handle();
 		render();
 	}
@@ -146,13 +112,74 @@ function handle() {
 	if ("t" in keysPressed) {
 		cache.goldbergAria.play();
 	}
-	if ("b" in keysPressed) {
-		cache.burp.play();
-	}
 }
 // Rendering other items
 function render() {
 	canvasContext.drawImage(cache.start, 0, 0, gameSize.x, gameSize.y);
 }
-// Start menu
+// State machine
+let stateMachine = new StateMachine({
+	init: "booting",
+	transitions: [
+		{name: "ready", from: "booting", to: "menu"},
+		{name: "pause", from: "menu", to: "paused"},
+		{name: "unpause", from: "paused", to: "menu"},
+	],
+	methods: {
+		onTransition: function (lifecycle) {
+			console.log("State: " + lifecycle.transition);
+		},
+		onBooting: function () {
+			// Draw loading screen
+			canvasContext.rect(0, 0, gameSize.x, gameSize.y);
+			canvasContext.fillStyle = "rgb(0, 0, 0)";
+			canvasContext.fill();
+			canvasContext.font = "240px \"Commodore 64\", \"Roboto Slab\", \"Century Gothic\", AppleGothic, sans-serif";
+			canvasContext.textAlign = "center";
+			canvasContext.fillStyle = "rgb(255, 255, 255)";
+			canvasContext.fillText("LOADING", gameSize.x / 2, gameSize.y / 2);
+		},
+		onReady: function () {
+			// Draw start menu
+			console.log(cache);
+			clear();
+			canvasContext.drawImage(cache.start, 0, 0, gameSize.x, gameSize.y);
+			cache.goldbergAria.play();
+			button(960, 640, "Easy", function () {
+				console.log("Easy difficulty");
+			}, 480);
+			button(960, 800, "Medium", function () {
+				console.log("Medium difficulty");
+			}, 480);
+			button(960, 960, "Hard", function () {
+				console.log("Hard difficulty");
+			}, 480);
+		},
+		onPause: function () {
+			canvasContext.rect(0, 0, gameSize.x, gameSize.y);
+			canvasContext.fillStyle = "rgba(0, 0, 0, 0.4)";
+			canvasContext.fill();
+			canvasContext.fillStyle = "rgb(255, 255, 255)";
+			canvasContext.fillText("PAUSED", gameSize.x / 2, gameSize.y / 2);
+			sounds.forEach(function (assetName, index) {
+				if (!cache[assetName].paused) {
+					cache[assetName].pause();
+					pausedAudio[index] = true;
+					console.log("PausedAudio[" + index + "], " + assetName + ", is now true.");
+				}
+			})
+		},
+		onUnpause: function () {
+			clear();
+			sounds.forEach(function (assetName, index) {
+				if (pausedAudio[index]) {
+					cache[assetName].play();
+					pausedAudio[index] = false;
+					console.log("PausedAudio[" + index + "], " + assetName + ", is now false.");
+				}
+			})
+		}
+	}
+});
+// Start
 loadResources(images, sounds);
