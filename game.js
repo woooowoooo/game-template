aa5b0bdea34c805a8cdd4cd2810dba5ac42dbf58
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2021-04-25 18:42:55 -0700

    Clickable buttons
    - Moved `button()` to be under `render()`.
        - Added click detection.
    - Redid `getMousePosition()`.
    - Added `main` state.
    - Removed `mousedown` event listener.
    - Fattened diagonal parts of the "no" symbol.
    - Removed now-unused `burp.mp3`.

--- game.js
+++ game.js
@@ -12,21 +12,6 @@ canvasContext.imageSmoothingEnabled = false;
 function clear() {
 	canvasContext.clearRect(0, 0, gameSize.x, gameSize.y);
 }
-function button(x, y, text, callback, width) {
-	canvasContext.font = "81.92px \"Commodore 64\", \"Roboto Slab\", \"Century Gothic\", AppleGothic, sans-serif";
-	console.log(canvasContext.measureText(text).width);
-	let buttonWidth = Math.ceil(canvasContext.measureText(text).width / 32) * 32;
-	if (width) {
-		buttonWidth = width - 160;
-	}
-	canvasContext.drawImage(cache.buttonStart, x - buttonWidth / 2 - 80, y, 80, 128);
-	canvasContext.drawImage(cache.buttonMiddle, x - buttonWidth / 2, y, buttonWidth, 128);
-	canvasContext.drawImage(cache.buttonEnd, x + buttonWidth / 2, y, 80, 128);
-	canvasContext.textAlign = "center";
-	canvasContext.fillStyle = "rgb(0, 0, 0)";
-	canvasContext.fillText(text, x, y + 92);
-	addEventListener("onclick", callback);
-}
 // Loading assets
 let cache = {};
 function loadResources(images, sounds) {
@@ -51,19 +36,18 @@ function loadResources(images, sounds) {
 		initialize("audio", "canplaythrough", "sounds/", assetName, ".mp3");
 	})
 }
-const images = ["start", "buttonStart", "buttonMiddle", "buttonEnd"];
+const images = ["start", "main", "buttonStart", "buttonMiddle", "buttonEnd"];
 const sounds = ["goldbergAria"];
 // Noting input
 let keysPressed = {};
-let clicked = false;
 let mousePosition = {
 	x: 0,
 	y: 0
 };
 function getMousePosition(event) {
-    let rect = canvas.getBoundingClientRect();
-    mousePosition.x = event.clientX - rect.left;
-    mousePosition.y = event.clientY - rect.top;
+    let bounds = canvas.getBoundingClientRect();
+    mousePosition.x = (event.clientX - bounds.left) * gameSize.x / (bounds.right - bounds.left);
+    mousePosition.y = (event.clientY - bounds.top) * gameSize.y / (bounds.bottom - bounds.top);
 }
 addEventListener("keydown", function (e) {
 	keysPressed[e.key] = true;
@@ -73,14 +57,9 @@ addEventListener("keyup", function (e) {
 	delete keysPressed[e.key];
 	console.log("The \"" + e.key + "\" key was released.");
 });
-addEventListener("mousedown", function (e) {
-	clicked = true;
+addEventListener("click", function (e) {
 	getMousePosition(e);
 	console.log("The mouse was clicked on " + mousePosition.x + ", " + mousePosition.y + ".");
-});
-addEventListener("mouseup", function (e) {
-	clicked = false;
-	console.log("The mouse was released.");
 });
 // Game loop
 function loop() {
@@ -113,17 +92,44 @@ function handle() {
 		cache.goldbergAria.play();
 	}
 }
-// Rendering other items
+// Rendering everything
 function render() {
-	canvasContext.drawImage(cache.start, 0, 0, gameSize.x, gameSize.y);
+	canvasContext.drawImage(cache.main, 0, 0, gameSize.x, gameSize.y);
+}
+function button(x, y, text, callback, width) {
+	// Determine width (font size is 8 * 1024 / 100)
+	canvasContext.font = "81.92px \"Commodore 64\", sans-serif";
+	let buttonWidth = Math.ceil(canvasContext.measureText(text).width / 32) * 32;
+	if (width) {
+		buttonWidth = width - 160;
+	}
+	// Draw button
+	canvasContext.drawImage(cache.buttonStart, x - buttonWidth / 2 - 80, y, 80, 128);
+	canvasContext.drawImage(cache.buttonMiddle, x - buttonWidth / 2, y, buttonWidth, 128);
+	canvasContext.drawImage(cache.buttonEnd, x + buttonWidth / 2, y, 80, 128);
+	canvasContext.textAlign = "center";
+	canvasContext.fillStyle = "rgb(0, 0, 0)";
+	canvasContext.fillText(text, x, y + 92);
+	// Detect clicks
+	let hitbox = new Path2D();
+	hitbox.rect(x - buttonWidth / 2 - 64, y, buttonWidth + 128, 128);
+	hitbox.rect(x - buttonWidth / 2 - 80, y + 16, buttonWidth + 160, 96);
+	hitbox.closePath();
+	addEventListener("click", function (e) {
+		getMousePosition(e);
+		if (canvasContext.isPointInPath(hitbox, mousePosition.x, mousePosition.y)) {
+			callback();
+		}
+	});
 }
 // State machine
 let stateMachine = new StateMachine({
 	init: "booting",
 	transitions: [
 		{name: "ready", from: "booting", to: "menu"},
-		{name: "pause", from: "menu", to: "paused"},
-		{name: "unpause", from: "paused", to: "menu"},
+		{name: "start", from: "menu", to: "main"},
+		{name: "pause", from: "main", to: "paused"},
+		{name: "unpause", from: "paused", to: "main"},
 	],
 	methods: {
 		onTransition: function (lifecycle) {
@@ -134,9 +140,9 @@ let stateMachine = new StateMachine({
 			canvasContext.rect(0, 0, gameSize.x, gameSize.y);
 			canvasContext.fillStyle = "rgb(0, 0, 0)";
 			canvasContext.fill();
-			canvasContext.font = "240px \"Commodore 64\", \"Roboto Slab\", \"Century Gothic\", AppleGothic, sans-serif";
-			canvasContext.textAlign = "center";
 			canvasContext.fillStyle = "rgb(255, 255, 255)";
+			canvasContext.font = "163.84px \"Commodore 64\", sans-serif";
+			canvasContext.textAlign = "center";
 			canvasContext.fillText("LOADING", gameSize.x / 2, gameSize.y / 2);
 		},
 		onReady: function () {
@@ -147,19 +153,27 @@ let stateMachine = new StateMachine({
 			cache.goldbergAria.play();
 			button(960, 640, "Easy", function () {
 				console.log("Easy difficulty");
+				stateMachine.start();
 			}, 480);
 			button(960, 800, "Medium", function () {
 				console.log("Medium difficulty");
+				stateMachine.start();
 			}, 480);
 			button(960, 960, "Hard", function () {
 				console.log("Hard difficulty");
+				stateMachine.start();
 			}, 480);
+		},
+		onStart: function() {
+			clear();
+			loop();
 		},
 		onPause: function () {
 			canvasContext.rect(0, 0, gameSize.x, gameSize.y);
 			canvasContext.fillStyle = "rgba(0, 0, 0, 0.4)";
 			canvasContext.fill();
 			canvasContext.fillStyle = "rgb(255, 255, 255)";
+			canvasContext.font = "163.84px \"Commodore 64\", sans-serif";
 			canvasContext.fillText("PAUSED", gameSize.x / 2, gameSize.y / 2);
 			sounds.forEach(function (assetName, index) {
 				if (!cache[assetName].paused) {
