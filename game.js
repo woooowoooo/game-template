6f1f4bcaf80e9c49ea2fa8007b35ab41bbf58e89
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-01-09 17:41:19 -0800

    Button classes
    - Generalized createButton slightly to encompass both mute buttons and text buttons
    - Changed buttonCallbacks into a full-fledged button array
    - Formalized drawing and handling the buttons
    - Moved click event listeners to only act on canvas
    - Moved main loop back out of onMain()

--- game.js
+++ game.js
@@ -14,13 +14,13 @@ const images = {};
 const sounds = {};
 let paused = false;
 let muted = false;
-const buttonCallbacks = {};
+const buttons = {};
 // Helper functions
 function clear() {
 	context.clearRect(0, 0, 1920, 1280);
-	for (const button in buttonCallbacks) {
-		removeEventListener("click", buttonCallbacks[button]);
-		delete buttonCallbacks[button];
+	for (const button in buttons) {
+		canvas.removeEventListener("click", buttons[button].fullCallback);
+		delete buttons[button];
 	}
 }
 function getMousePosition(event) {
@@ -31,63 +31,78 @@ function getMousePosition(event) {
 function setFontSize(size) {
 	context.font = `${size * 1024 / 100}px "Commodore 64", sans-serif`;
 }
-function wrapClickEvent(listenerID, callback, condition = (() => true)) {
+function wrapClickEvent(callback, condition = (() => true)) {
 	// TODO: Figure out a way to use {once: true}
 	function fullCallback(e) {
 		if (condition(e)) {
 			callback();
-			removeEventListener("click", fullCallback);
+			canvas.removeEventListener("click", fullCallback);
 		}
 	};
-	addEventListener("click", fullCallback);
-	buttonCallbacks[listenerID] = fullCallback;
+	canvas.addEventListener("click", fullCallback);
+	return fullCallback;
 }
 // Buttons
-function createButton(id, x, y, dx, dy, imagePath, callback) {
-	context.drawImage(images[imagePath], x, y, dx, dy);
-	const hitbox = new Path2D();
-	hitbox.rect(x, y, dx, dy);
-	hitbox.closePath();
-	wrapClickEvent(id, callback, () => context.isPointInPath(hitbox, mouse.x, mouse.y) && !paused);
+class Button {
+	constructor (id, hitbox, draw, callback, ignorePause = false) {
+		this.id = id;
+		this.callback = callback;
+		this.draw = draw;
+		this.hitbox = hitbox;
+		this.fullCallback = wrapClickEvent(callback, () => context.isPointInPath(hitbox, mouse.x, mouse.y) && (!paused || ignorePause));
+		draw();
+	}
 }
-function muteButton() {
-	createButton(muted ? "unmute" : "mute", 1920 - 96, 1280 - 96, 96, 96, muted ? "soundOff" : "soundOn", function () {
-		muted = !muted;
-		console.log(muted ? "Muted" : "Unmuted");
-		for (const sound of Object.values(sounds)) {
-			sound.muted = muted;
+class MuteButton extends Button {
+	constructor () {
+		const [x, y, dx, dy] = [1920 - 96, 1280 - 96, 96, 96];
+		const name = muted ? "unmute" : "mute";
+		const hitbox = new Path2D();
+		hitbox.rect(x, y, dx, dy);
+		hitbox.closePath();
+		function draw() {
+			context.drawImage(images[muted ? "soundOff" : "soundOn"], x, y, dx, dy);
 		}
-		removeEventListener("click", buttonCallbacks[muted ? "soundOff" : "soundOn"]);
-		delete buttonCallbacks[muted ? "soundOff" : "soundOn"];
-	});
+		function callback() {
+			muted = !muted;
+			console.log(muted ? "Muted" : "Unmuted");
+			for (const sound of Object.values(sounds)) {
+				sound.muted = muted;
+			}
+			delete buttons[name];
+			buttons[muted ? "unmute" : "mute"] = new MuteButton();
+		}
+		super(name, hitbox, draw, callback);
+	}
 }
-function textButton(x, y, text, callback, width, ignorePause = false) {
-	setFontSize(8);
-	const buttonWidth = width ? width - 160 : Math.ceil(context.measureText(text).width / 32) * 32;
-	// Draw button
-	context.drawImage(images.buttonStart, x - buttonWidth / 2 - 80, y, 80, 128);
-	context.drawImage(images.buttonMiddle, x - buttonWidth / 2, y, buttonWidth, 128);
-	context.drawImage(images.buttonEnd, x + buttonWidth / 2, y, 80, 128);
-	context.textAlign = "center";
-	context.fillStyle = "rgb(0, 0, 0)";
-	context.fillText(text, x, y + 92);
-	// Detect clicks
-	const hitbox = new Path2D();
-	hitbox.rect(x - buttonWidth / 2 - 64, y, buttonWidth + 128, 128);
-	hitbox.rect(x - buttonWidth / 2 - 80, y + 16, buttonWidth + 160, 96);
-	hitbox.closePath();
-	wrapClickEvent(text.toLowerCase(), callback, () => context.isPointInPath(hitbox, mouse.x, mouse.y) && (!paused || ignorePause));
+class TextButton extends Button {
+	constructor (x, y, text, callback, width, ignorePause = false) {
+		const buttonWidth = width ? width - 160 : Math.ceil(context.measureText(text).width / 32) * 32;
+		const hitbox = new Path2D();
+		hitbox.rect(x - buttonWidth / 2 - 64, y, buttonWidth + 128, 128);
+		hitbox.rect(x - buttonWidth / 2 - 80, y + 16, buttonWidth + 160, 96);
+		hitbox.closePath();
+		function draw() {
+			setFontSize(8);
+			context.drawImage(images.buttonStart, x - buttonWidth / 2 - 80, y, 80, 128);
+			context.drawImage(images.buttonMiddle, x - buttonWidth / 2, y, buttonWidth, 128);
+			context.drawImage(images.buttonEnd, x + buttonWidth / 2, y, 80, 128);
+			context.textAlign = "center";
+			context.fillStyle = "rgb(0, 0, 0)";
+			context.fillText(text, x, y + 92);
+		}
+		super(text.toLowerCase(), hitbox, draw, callback, ignorePause);
+	}
 }
 // Noting input
-addEventListener("keydown", e => {
+window.addEventListener("keydown", e => {
 	keysPressed.add(e.key);
 	console.log(`The "${e.key}" key was pressed.`);
 });
-addEventListener("keyup", e => {
+window.addEventListener("keyup", e => {
 	keysPressed.delete(e.key);
-	console.log(`The "${e.key}" key was released.`);
 });
-addEventListener("click", e => {
+canvas.addEventListener("click", e => {
 	getMousePosition(e);
 	console.log("The mouse was clicked on " + mouse.x + ", " + mouse.y + ".");
 });
@@ -143,7 +158,7 @@ const stateMachine = new StateMachine({
 	],
 	methods: {
 		onTransition(lifecycle) {
-			console.log(`Transition: ${lifecycle.transition}\nNew State: ${lifecycle.to}`);
+			console.log(`Transition: ${lifecycle.transition}\tNew State: ${lifecycle.to}`);
 		},
 		async onBoot() {
 			// Loading screen
@@ -169,35 +184,26 @@ const stateMachine = new StateMachine({
 			context.fillText("Loading finished.", 960, 400);
 			context.fillText("CLICK ANYWHERE", 960, 800);
 			context.fillText("TO CONTINUE", 960, 960);
-			wrapClickEvent("autoplayPrompt", stateMachine.toMenu);
+			wrapClickEvent(stateMachine.toMenu);
 		},
 		onMenu() {
 			clear();
 			context.drawImage(images.start, 0, 0, 1920, 1280);
 			sounds.mainTheme.play();
-			textButton(960, 720, "Start", stateMachine.start, 576);
-			textButton(960, 912, "Credits", stateMachine.toCredits, 576);
-			muteButton();
+			buttons.start = new TextButton(960, 720, "Start", stateMachine.start, 576);
+			buttons.credits = new TextButton(960, 912, "Credits", stateMachine.toCredits, 576);
+			buttons[muted ? "unmute" : "mute"] = new MuteButton();
 		},
 		onCredits() {
 			clear();
 			context.drawImage(images.credits, 0, 0, 1920, 1280);
-			textButton(960, 912, "Return", stateMachine.toMenu, 576);
-			muteButton();
+			buttons.return = new TextButton(960, 912, "Return", stateMachine.toMenu, 576);
+			buttons[muted ? "unmute" : "mute"] = new MuteButton();
 		},
 		onMain() {
-			// Main loop
-			if (stateMachine.is("main")) {
-				// Render
-				clear();
-				context.drawImage(images.main, 0, 0, 1920, 1280);
-				muteButton();
-				// Handle inputs
-				if (keysPressed.has("p") || keysPressed.has("P") || keysPressed.has("Escape")) {
-					stateMachine.pause();
-				}
-				requestAnimationFrame(stateMachine.onMain);
-			}
+			clear();
+			buttons[muted ? "unmute" : "mute"] = new MuteButton();
+			requestAnimationFrame(loop);
 		},
 		onPaused() {
 			paused = true;
@@ -210,8 +216,8 @@ const stateMachine = new StateMachine({
 			setFontSize(16);
 			context.fillStyle = "rgb(255, 255, 255)";
 			context.fillText("PAUSED", 960, 400);
-			textButton(672, 880, "Menu", stateMachine.toMenu, 480, true);
-			textButton(1248, 880, "Return", stateMachine.unpause, 480, true);
+			buttons.menu = new TextButton(672, 880, "Menu", stateMachine.toMenu, 480, true);
+			buttons.return = new TextButton(1248, 880, "Return", stateMachine.unpause, 480, true);
 		},
 		onLeavePaused() {
 			paused = false;
@@ -221,3 +227,19 @@ const stateMachine = new StateMachine({
 		}
 	}
 });
+// Main loop
+function loop() {
+	if (!stateMachine.is("main")) {
+		return;
+	}
+	// Render
+	context.clearRect(0, 0, 1920, 1280);
+	context.drawImage(images.main, 0, 0, 1920, 1280);
+	buttons[muted ? "unmute" : "mute"].draw();
+	// Handle inputs
+	if (keysPressed.has("p") || keysPressed.has("P") || keysPressed.has("Escape")) {
+		stateMachine.pause();
+		return;
+	}
+	requestAnimationFrame(loop);
+}
